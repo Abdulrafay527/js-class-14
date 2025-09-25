bc837bf0b642b0db7878e40b2da5ddb04b39d356
Abdulrafay527 <rafaymoosait@gmail.com>
2025-09-25 15:55:05 +0500

    js-class-14 worked

---
+++ app.js
@@ -0,0 +1,15 @@
+// var date = new Date();
+// var days = ["sun" , "mon" , "tue" , "wed" , "thu" , "fir" , "sat"]
+// console.log(days[date.getDay()]);
+// console.log(date.getHours());
+// console.log(date.getUTCMinutes());
+// console.log(date.getSeconds());
+
+
+
+// var date = new Date();
+// var days = ["Sun" , "Mon" , "Tue" , "Wed" , "Thu" , "Fri" , "Sat"]
+// console.log(days[date.getDay()]);
+// console.log(date.getHours());
+// console.log(date.getUTCMinutes());
+// console.log(date.getSeconds());
