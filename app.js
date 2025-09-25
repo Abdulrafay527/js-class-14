da4b3aa329b0307b10a38ebe43632ef16a6d1020
Abdulrafay527 <rafaymoosait@gmail.com>
2025-09-25 19:56:20 +0500

    js-class-14 work completed

--- app.js
+++ app.js
@@ -13,3 +13,28 @@
 // console.log(date.getHours());
 // console.log(date.getUTCMinutes());
 // console.log(date.getSeconds());
+
+
+// function abc(){
+//     console.log(new Date())
+// }
+
+// abc();
+
+
+
+// var userInput = +prompt("enter your num");
+
+// function myString(num){
+//     console.log(num.toString())
+// }
+
+// myString(userInput)
+
+
+document.getElementById("main").innerHTML = "rafay moosa";
+
+function getValue(){
+    var input = document.getElementById("input").value
+    console.log(input)
+}
